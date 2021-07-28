960a93be217647225c0cf0b3fe268c7ace66717f
Marco Schulte <marco.schulte@eckert-partner.it>
2021-07-28 23:49:49 +0200

    Fix co2 aqi calculation

--- src/AQICalculator.h
+++ src/AQICalculator.h
@@ -9,7 +9,7 @@ public:
 
     const float thresholdsPM2_5[8] = {0, 12, 35.5, 55.5, 150.5, 250.5, 350.5, 500.5};
     const float thresholdsPM10_0[8] = {0, 55, 155, 255, 355, 425, 505, 605};
-    const float thresholdsCO2[8] = {0, 800, 1000, 1500, 2000, 5000, 8000, 1200};
+    const float thresholdsCO2[8] = {0, 800, 1000, 1500, 2000, 5000, 8000, 10000};
 
 private:
     const int minLevels[8] = {0, 50, 100, 150, 200, 300, 400, 500};
